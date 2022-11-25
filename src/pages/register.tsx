41143505c6cff74a6fc85ed7b8bab914fb08c9fe
JosseMontano <eljosema505@gmail.com>
2022-11-25 11:41:54 -0400

    instalando dependencias

---
+++ src/pages/register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
+
+//nombre, numero d etelfono, ci, direccion exacta, vivienda, numero de emeergencia
